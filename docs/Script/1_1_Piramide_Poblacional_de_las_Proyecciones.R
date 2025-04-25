8b85197a25409a775b1df9fa2d0f0345438e2c5c
Nicolás García Peñaloza. <77413779+NicolasGP01@users.noreply.github.com>
2025-04-25 16:25:20 -0500

    Update 1_1_Piramide_Poblacional_de_las_Proyecciones.R

--- docs/Script/1_1_Piramide_Poblacional_de_las_Proyecciones.R
+++ docs/Script/1_1_Piramide_Poblacional_de_las_Proyecciones.R
@@ -22,30 +22,28 @@ paquetes = c('devtools' ,
              'skimr' ,  'readxl' , 
              'stringr' , 'stringi' , 'reshape' ,
              'viridis' ,'tidyverse'
-             )
+)
 
 
 print("IMPORTANTE!! Una vez se asegura que esten instalados llamar la libreria")
 
 sapply(paquetes,require,character.only=T) ; rm(paquetes )
 
-
-
 ### SECCIÓN DE POBLACIÓN Y PROYECCIONES -----------------------
 ### Base de datos de 1985 - 1994
-POBLA_1985 = read_excel("C:/Users/nicol.NICOLAS_GP/Dropbox/Data Bases/Población/Proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-1985-1994.xlsx" , range = "A12:JG33672") |> 
+POBLA_1985 = read_excel("C:/Users/ANA MARIA/Downloads/Bases de datos/Poblacion - proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-1985-1994.xlsx" , range = "A12:JG33672") |> 
   data.frame() |> dplyr::rename( "MPIO_CDPMP" = "DPMP" , "MUNICIPIO" = "MPIO")
 
 #### Base de datos de 1995 - 2004
-POBLA_1995 = read_excel("C:/Users/nicol.NICOLAS_GP/Dropbox/Data Bases/Población/Proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-1995-2004.xlsx" , range = "A12:JG33672") |> 
+POBLA_1995 = read_excel("C:/Users/ANA MARIA/Downloads/Bases de datos/Poblacion - proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-1995-2004.xlsx", range = "A12:JG33672") |> 
   data.frame() |> dplyr::rename( "MPIO_CDPMP" = "DPMP" , "MUNICIPIO" = "MPIO")
 
 #### Base de datos de 2005 - 2019
-POBLA_2005 = read_excel( "C:/Users/nicol.NICOLAS_GP/Dropbox/Data Bases/Población/Proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-2005-2019.xlsx" , range = "A12:JG50502") |> 
+POBLA_2005 = read_excel("C:/Users/ANA MARIA/Downloads/Bases de datos/Poblacion - proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-2005-2019.xlsx" , range = "A12:JG50502") |> 
   data.frame() |> dplyr::rename( "MPIO_CDPMP" = "MPIO" , "MUNICIPIO" = "DPMP" )
 
 #### Base de datos de 2020 - 2035
-POBLA_2020 = read_excel( "C:/Users/nicol.NICOLAS_GP/Dropbox/Data Bases/Población/Proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-2020-2035-ActPostCOVID-19.xlsx" , range = "A9:JG53865") |> 
+POBLA_2020 = read_excel( "C:/Users/ANA MARIA/Downloads/Bases de datos/Poblacion - proyecciones/DCD-area-sexo-edad-proypoblacion-Mun-2020-2035-ActPostCOVID-19.xlsx", range = "A9:JG53865") |> 
   data.frame() |> dplyr::rename( "MPIO_CDPMP" = "MPIO" , "MUNICIPIO" = "DPMP" )
 
 print("IMPORTANTE!! Tomar en cuenta que los nombres de los vectores esten homogenizados.")
@@ -59,38 +57,37 @@ head(POBLA)
 str(POBLA , list.len= 268)
 skimr::skim(POBLA)
 
-?str()
-
-table(POBLA$ÁREA.GEOGRÁFICA , useNA = "always")
-
+table(POBLA$ÁREA.GEOGRÁFICA , useNA = "always") ## Dame una tabla de frecuencia de la variable Área Geografica para ver cuántas Áreas tengo (Tenga en cuenta lo valores perdidos si es que los hay)
+table(POBLA$MUNICIPIO , useNA = "always") ## Dame una tabla de frecuencia de la variable Municipios para ver cuántas Áreas tengo (Tenga en cuenta lo valores perdidos si es que los hay)
+table(POBLA$MPIO_CDPMP , useNA = "always") ## Dame una tabla de frecuencia de la variable Municipios para ver cuántas Áreas tengo (Tenga en cuenta lo valores perdidos si es que los hay)
 
 ## depuración --------
-POBLA = POBLA |>  dplyr::rename("Mujeres_85" = "Mujeres_85.y.más" , "Hombres_85" = "Hombres_85.y.más") |> 
+POBLA = POBLA |>  dplyr::rename("Mujeres_85" = "Mujeres_85.y.más" , "Hombres_85" = "Hombres_85.y.más") |> ## Renombramos variables
   dplyr::select(-starts_with("Total")) |> ## Filtramos todos aquellos vectores que inician con Total.
-   dplyr::filter(MPIO_CDPMP == "63001") |> ## Dejamos solo Armenia.
+  dplyr::filter(MPIO_CDPMP == "63001") |> ## Dejamos solo Armenia. #Por qué lo hago con el código y no con el nombre del municipio?
   dplyr::select(-c(DP , DPNOM , MUNICIPIO)) ## Solo lar variables que necesitamos.
 
 
 ## Transponemos la base de datos. 
-POBLA = POBLA |> reshape::melt( id=c("MPIO_CDPMP" , "ÁREA.GEOGRÁFICA" ,"AÑO" ) , variable_name = "EDAD"  )
+POBLA = POBLA |> reshape::melt( id=c("MPIO_CDPMP" , "ÁREA.GEOGRÁFICA" ,"AÑO" ) , variable_name = "EDAD"  ) ## Transponemos la base de datos para que me quede el registro tanto del sexo como de la edad en formato largo
 
 # Separamos.
-POBLA = POBLA |> separate(EDAD, c("SEXO", "EDAD" ), "_")
+POBLA = POBLA |> separate(EDAD, c("SEXO", "EDAD" ), "_") ## Separamos la variable de EDAD que contiene no solo la edad si no tamnién el sexo 
 
 
 # Proyecciones Historicas-------------
-print("1- Población total")
-POBLA |> dplyr::filter( ÁREA.GEOGRÁFICA == "Total" ) |> group_by(AÑO) |> 
-  summarise(POPULATION = sum(value)) |> #View()
+print("1- Serie de la Población total")
+POBLA |> dplyr::filter( ÁREA.GEOGRÁFICA == "Total" ) |> group_by(AÑO) |> ##Filtramos el Total y agrupamos por el año par acontar la poblacional anual
+  summarise(POPULATION = sum(value)) |> #View() ## Como agrupamos debemos decir qué queremos una suma de esos valores correspondientes a la variable que toma el total de personas
   ggplot( aes(x = as.Date(as.character( paste0( as.character(AÑO) , "-12-31"))) , y = POPULATION )) + 
-  geom_line(colour = "#26619C", size = 1) +
-  scale_x_date(date_breaks = "5 year", 
+  geom_line(colour = "#26619C", size = 1) + ## El gráfico será una linea especificando el color y el grosor
+  scale_x_date(date_breaks = "5 year",  ## Vamos a darle formato al eje X
                #labels=date_format("%Y"),
                limits = as.Date(c('1985-12-31','2035-12-31')), date_labels="%Y" ) +
   labs(title = "",
        x = "Año",
-       y = "Población") +
-  theme_minimal()
+       y = "Población") + 
+  theme_minimal() ## Especifiquemos un tema de la gráfica
 
 
 print("2- Población total según sexo") 
@@ -135,12 +132,12 @@ POBLA_1 = POBLA |> dplyr::rename( "AREA" = "ÁREA.GEOGRÁFICA") |>
 
 ## Si le aparece un error que le exprese que no encuentra la variable "AREA" o "ÁREA.GEOGRÁFICA en el código anterior digite el siguiente código.
 #POBLA_1 = POBLA |> 
- #   dplyr::rename("AREA" = "ÁREA.GEOGRÁFICA") |> 
-  #  dplyr::filter(AREA == "Total" & AÑO == 1985) |> 
-   # dplyr::group_by(MPIO_CDPMP, AREA, AÑO, GENERO, EDAD) |> 
-    #dplyr::summarise(PERSONAS = sum(value)) |> 
-     #dplyr::ungroup()
-  
+#   dplyr::rename("AREA" = "ÁREA.GEOGRÁFICA") |> 
+#  dplyr::filter(AREA == "Total" & AÑO == 1985) |> 
+# dplyr::group_by(MPIO_CDPMP, AREA, AÑO, GENERO, EDAD) |> 
+#dplyr::summarise(PERSONAS = sum(value)) |> 
+#dplyr::ungroup()
+
 
 # Gráfica
 print("3- Priamide poblacional de Armenia para el año 1985" ) 
@@ -157,5 +154,5 @@ ggplot(POBLA_1 ,aes(x =EDAD , y = PERSONAS ,fill = SEXO )) +
   theme_minimal()
 
 
-print("Quiz: Seleccione el municipio de xxxx y saque la piramide para el año xxxx")
+print("Quiz: Seleccione el municipio de Genova-Quindío y saque la piramide para el año 1997")
 
